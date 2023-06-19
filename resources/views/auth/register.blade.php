a721a08aab1c09a8c7d9bfa2413229322382ed17
Ruby Mbete <108268034+r-mbete@users.noreply.github.com>
2023-06-20 01:09:30 +0300

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,12 @@
 <x-guest-layout>
+
+    <style>
+        body{
+            background-image: url('background_image1.jpg');
+            background-size: 100%;
+        }
+    </style>
+    
     <form method="POST" action="{{ route('register') }}">
         @csrf
 <!-- component -->
